322c9db6295b7de3a773cf9bcfd95ca9372f43eb
moradan <realmoradan@hotmail.com>
2024-08-16 12:19:15 -0300

    Presentacion nombre + fondo azul

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import Col from 'react-bootstrap/col'
+import Row from 'react-bootstrap/row';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className='fw-light'>&lt;Rodrigo Gimenez&gt;</h1>
-      </header>
+    <div className="App container-fluid">
+      <Row>
+        <Col xs={6}>
+          <header className="App-header">
+
+            <h1 className='fw-light'>&lt;Rodrigo Gimenez&gt;</h1>
+            <span className='fw-light'>Desarrollador web</span>
+          </header>
+        </Col>
+      </Row>
     </div>
   );
 }
